d7cfab8027b6ffc90dd33a5411bab3e05dcd519e
Manuel Mejia <maalmer@gmail.com>
2023-06-20 15:14:45 -0700

    Website updated 2023-2

---
+++ script.R
@@ -0,0 +1,25 @@
+
+# Check book:
+# https://crumplab.com/cognition/textbook/index.html
+# Clean files for activities:
+# 
+
+
+# Render book ---------------------------------------------------
+
+system("quarto render")
+system("quarto render --profile website")
+
+
+# Copy book into git folder -------------------------------------
+
+path_to_2 <- "_site/2023-2/"
+
+# Push book into github -----------------------------------------
+
+setwd(path_to_2)
+system("git add .")
+message <- "Website updated 2023-2"
+system(paste0("git commit -m \"", message, "\" "))
+system("git push origin main")
+setwd("../../")
