48e62b113c344b86eda6fe49e1e7ce85fc579cf2
Manuel Mejia <maalmer@gmail.com>
2024-07-29 20:49:03 -0700

    Website updated 2024-2

--- script.R
+++ script.R
@@ -13,13 +13,13 @@ system("quarto render --profile website")
 
 # Copy book into git folder -------------------------------------
 
-#path_to_2 <- "docs/2023-2/"
+#path_to_2 <- "docs/2024-2/"
 
 # Push book into github -----------------------------------------
 
 #setwd(path_to_2)
 system("git add .")
-message <- "Website updated 2023-2"
+message <- "Website updated 2024-2"
 system(paste0("git commit -m \"", message, "\" "))
 system("git push origin main")
 #setwd("../../")
